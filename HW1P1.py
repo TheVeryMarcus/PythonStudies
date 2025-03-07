745b89bf24401ec330c30df94d148d302335d573
The Very Marcus <theverymarcus@gmail.com>
2025-03-07 20:10:31 +0300

    Add files via upload

---
+++ HW1P1.py
@@ -0,0 +1,5 @@
+num1 = 100
+name = "Алексей"
+first_number = 1
+text = "Мой текст"
+message = "Hi!"
---
+++ HW1P3.py
@@ -0,0 +1,7 @@
+# ---------------- Задача 3 ----------------
+
+name = input()
+surname = input()
+
+print(f"Привет, меня зовут {name.title()} {surname.title()}.")
+
